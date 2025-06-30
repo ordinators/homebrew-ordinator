ec6cc30119d5ea26d65ee8fe568c35cf4870427a
Anthony Norfleet <anorfleet@trellahealth.com>
2025-06-29 22:15:16 -0400

    fix: update SHA256 for v0.1.2 with Cargo.lock included

--- Formula/ordinator.rb
+++ Formula/ordinator.rb
@@ -2,7 +2,7 @@ class Ordinator < Formula
   desc "Dotfiles and Environment Manager for macOS"
   homepage "https://github.com/ordinators/ordinator"
   url "https://github.com/ordinators/ordinator/archive/v0.1.2.tar.gz"
-  sha256 "2355872b6cb2470a1c42d0b86945aac48da0c7896db8ddd129b1990939c39202"
+  sha256 "15ac211c24fc8d84bfff84a57aa98a5363569d394bcc6b9297d6583c8780ac5c"
   license "MIT"
   head "https://github.com/ordinators/ordinator.git", branch: "master"
 
