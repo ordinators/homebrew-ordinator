f0e1593b00d129a72f556731ec65b0b3bb492550
Anthony Norfleet <anorfleet@trellahealth.com>
2025-06-30 09:57:12 -0400

    chore: use standard Homebrew Rust formula pattern

--- Formula/ordinator.rb
+++ Formula/ordinator.rb
@@ -9,14 +9,7 @@ class Ordinator < Formula
   depends_on "rust" => :build
 
   def install
-    # Override CPU target and add compilation optimizations
-    ENV["RUSTFLAGS"] = "-C target-cpu=native -C codegen-units=1"
-    ENV["CARGO_NET_TIMEOUT"] = "300"
-    ENV["CARGO_BUILD_JOBS"] = ENV.make_jobs.to_s
-    
-    # Use cargo build instead of install for more control
-    system "cargo", "build", "--release", "--locked", "--verbose"
-    bin.install "target/release/ordinator"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
