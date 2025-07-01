e80731c788824aa57e97fbc8596b5d147f309a79
Anthony Norfleet <anorfleet@trellahealth.com>
2025-06-30 22:39:12 -0400

    Fix Homebrew formula: use refs/tags in url and remove trailing whitespace

--- Formula/ordinator.rb
+++ Formula/ordinator.rb
@@ -1,7 +1,7 @@
 class Ordinator < Formula
   desc "Dotfiles and Environment Manager for macOS"
   homepage "https://github.com/ordinators/ordinator"
-  url "https://github.com/ordinators/ordinator/archive/v0.1.2.tar.gz"
+  url "https://github.com/ordinators/ordinator/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "17b4ec9d3411a4f50c0453f355e909df29c0d7b81026338410c2a54545cde23a"
   license "MIT"
   head "https://github.com/ordinators/ordinator.git", branch: "master"
