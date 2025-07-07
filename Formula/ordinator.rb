fa1cac12d10599eeaa295e581d6b7bef9dfef07c
Anthony Norfleet <anorfleet@trellahealth.com>
2025-07-07 13:40:28 -0400

    Update ordinator to v0.3.1 and add SOPS/age dependencies

--- Formula/ordinator.rb
+++ Formula/ordinator.rb
@@ -1,12 +1,14 @@
 class Ordinator < Formula
   desc "Dotfiles and Environment Manager for macOS"
   homepage "https://github.com/ordinators/ordinator"
-  url "https://github.com/ordinators/ordinator/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/ordinators/ordinator/archive/refs/tags/v0.3.1.tar.gz"
   sha256 "ba5c34e3217319aa0130fd040ef43ec482d6c5413acd0563de28663be9b33d06"
   license "MIT"
   head "https://github.com/ordinators/ordinator.git", branch: "master"
 
   depends_on "rust" => :build
+  depends_on "sops"
+  depends_on "age"
 
   def install
     # Set OpenSSL environment variables to use Homebrew's OpenSSL
