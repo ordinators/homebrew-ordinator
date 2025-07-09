8544d89192dbbf90fdb6fd4d8f05da6c69d5f15f
Anthony Norfleet <anthony.norfleet@gmail.com>
2025-07-09 17:24:54 -0400

    Update ordinator to v0.4.2 (#10)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/ordinator.rb
+++ Formula/ordinator.rb
@@ -1,8 +1,8 @@
 class Ordinator < Formula
   desc "Dotfiles and Environment Manager for macOS"
   homepage "https://github.com/ordinators/ordinator"
-  url "https://github.com/ordinators/ordinator/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "383b8b5f11085a88616b1b681598fbee63804034d48da64e4b5b14a0567d026d"
+  url "https://github.com/ordinators/ordinator/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "f27d512ca98068b61fd197a0f1908e0c9ae7ed94942580aceb88c0d4f8f333e6"
   license "MIT"
   head "https://github.com/ordinators/ordinator.git", branch: "master"
 
